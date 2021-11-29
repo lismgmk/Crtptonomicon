570f7b521088a357597c08eefcd472948fe7e020
lismgmk <lismgmk2@gmail.com>
2021-11-29 19:08:04 +0300

    second

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
           >Тикер</label
           >
           <div class="mt-1 relative rounded-md shadow-md">
-            {{ value }}
             <input
                 @keydown.enter = "addCurrency"
                 v-model="value"
@@ -44,7 +43,6 @@
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
       >
-        <!-- Heroicon name: solid/mail -->
         <svg
             class="-ml-0.5 mr-2 h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -60,21 +58,25 @@
         Добавить
       </button>
     </section>
+
 <template v-if="currencies.length">
   <hr class="w-full border-t border-gray-600 my-4"/>
   <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-
     <div
+        @click="selected(currency)"
         :key="index"
         v-for="(currency, index) in currencies"
-        class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+        :class="{
+            'border-4': sel === currency
+        }"
+        class="bg-white  overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
     >
       <div class="px-4 py-5 sm:p-6 text-center">
         <dt class="text-sm font-medium text-gray-500 truncate">
-          {{currency.value}} - USD
+          {{currency.name}} - USD
         </dt>
         <dd class="mt-1 text-3xl font-semibold text-gray-900">
-          1.11
+          {{currency.price}}
         </dd>
       </div>
       <div class="w-full border-t border-gray-200"></div>
@@ -104,27 +106,22 @@
 </template>
 
 
-    <section class="relative">
+    <section class="relative" v-if="sel">
       <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
         VUE - USD
       </h3>
       <div class="flex items-end border-gray-600 border-b border-l h-64">
         <div
-            class="bg-purple-800 border w-10 h-24"
-        ></div>
-        <div
-            class="bg-purple-800 border w-10 h-32"
-        ></div>
-        <div
-            class="bg-purple-800 border w-10 h-48"
-        ></div>
-        <div
-            class="bg-purple-800 border w-10 h-16"
+            :key="index"
+            v-for="(gr, index) in roundGraph()"
+            :style="{ height: gr+'%'}"
+            class="bg-purple-800 border w-10"
         ></div>
       </div>
       <button
           type="button"
           class="absolute top-0 right-0"
+          @click="sel = null"
       >
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -156,18 +153,45 @@ export default {
   data() {
     return {
       value: '',
-      currencies: []
+      currencies: [],
+      sel: null,
+      interval: null,
+      graph: []
     }
   },
   methods: {
     addCurrency() {
-      const newCurrency = {id: this.value, name: this.value}
+      const newCurrency = {price: '-', name: this.value}
       this.currencies.push(newCurrency)
+
+      this.interval =  setInterval(async ()=> {
+        let request = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${newCurrency.name}&tsyms=USD&api_key=d52aa675ecbb2b0e1541ef3f89f1efbe2ab21a3bd943ef86e6041794d25b9841`)
+
+        const data = await request.json()
+        console.log(data.USD)
+        this.currencies[0].price = data
+        this.graph.push(data)
+        console.log(this.graph)
+      }, 3000)
+
       this.value = ''
     },
     deleteCurrency(cur) {
       this.currencies = this.currencies.filter(currency => currency !== cur)
+      this.sel = null
+    },
+    selected(currency){
+      this.sel = currency
+      clearInterval(this.interval)
+    },
+    roundGraph(){
+      const maxVal = Math.max(this.graph)
+      const minVal = Math.min(this.graph)
+      return  this.graph.map((gr) => {
+        return  5+(gr - minVal) * 100/(maxVal - minVal)
+      })
     }
+
   }
 }
 </script>
