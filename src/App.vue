ec3e7fc2ba36aeac39e90036e11be8a23a61be50
lismgmk <lismgmk2@gmail.com>
2021-11-30 18:46:29 +0300

    add filter need fix

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
           >
           <div class="mt-1 relative rounded-md shadow-md">
             <input
-                @keydown.enter = "addCurrency"
+                @keydown.enter="addCurrency"
                 v-model="value"
                 type="text"
                 name="wallet"
@@ -59,56 +59,56 @@
       </button>
     </section>
 
-<template v-if="currencies.length">
-  <hr class="w-full border-t border-gray-600 my-4"/>
-  <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-    <div
-        @click="selected(currency)"
-        :key="index"
-        v-for="(currency, index) in currencies"
-        :class="{
+    <template v-if="currencies.length">
+      <hr class="w-full border-t border-gray-600 my-4"/>
+      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        <div
+            @click="selected(currency)"
+            :key="index"
+            v-for="(currency, index) in currencies"
+            :class="{
             'border-4': sel === currency
         }"
-        class="bg-white  overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-    >
-      <div class="px-4 py-5 sm:p-6 text-center">
-        <dt class="text-sm font-medium text-gray-500 truncate">
-          {{currency.name}} - USD
-        </dt>
-        <dd class="mt-1 text-3xl font-semibold text-gray-900">
-          {{currency.price}}
-        </dd>
-      </div>
-      <div class="w-full border-t border-gray-200"></div>
-      <button
-          @click.stop="deleteCurrency(currency)"
-          class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-      >
-        <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="#718096"
-            aria-hidden="true"
+            class="bg-white  overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
         >
-          <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"
-          ></path>
-        </svg>
-        Удалить
-      </button>
-    </div>
+          <div class="px-4 py-5 sm:p-6 text-center">
+            <dt class="text-sm font-medium text-gray-500 truncate">
+              {{ currency.name }} - USD
+            </dt>
+            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+              {{ currency.price }}
+            </dd>
+          </div>
+          <div class="w-full border-t border-gray-200"></div>
+          <button
+              @click.stop="deleteCurrency(currency)"
+              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+          >
+            <svg
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="#718096"
+                aria-hidden="true"
+            >
+              <path
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+              ></path>
+            </svg>
+            Удалить
+          </button>
+        </div>
 
-  </dl>
-  <hr class="w-full border-t border-gray-600 my-4"/>
-</template>
+      </dl>
+      <hr class="w-full border-t border-gray-600 my-4"/>
+    </template>
 
 
     <section class="relative" v-if="sel">
       <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-        {{sel.name}} - USD
+        {{ sel.name }} - USD
       </h3>
       <div class="flex items-end border-gray-600 border-b border-l h-64">
         <div
@@ -156,7 +156,8 @@ export default {
       currencies: [],
       sel: null,
       interval: null,
-      graph: []
+      graph: [],
+      allCrypto: []
     }
   },
   methods: {
@@ -164,21 +165,20 @@ export default {
       const newCurrency = {price: '-', name: this.value}
       this.currencies.push(newCurrency)
 
-      this.interval =  setInterval(async ()=> {
+      this.interval = setInterval(async () => {
         let request = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${newCurrency.name}&tsyms=USD&api_key=d52aa675ecbb2b0e1541ef3f89f1efbe2ab21a3bd943ef86e6041794d25b9841`)
 
         const data = await request.json()
 
 
-
         this.currencies.map(cur => {
-          if(cur.name === newCurrency.name){
+          if (cur.name === newCurrency.name) {
             return cur.price = data.USD < 1 ? data.USD.toPrecision(2) : data.USD.toFixed(2)
           } else {
             return cur
           }
         })
-        if(this.sel?.name === newCurrency.name){
+        if (this.sel?.name === newCurrency.name) {
 
           this.graph.push(data.USD)
         }
@@ -193,23 +193,38 @@ export default {
       this.currencies = this.currencies.filter(currency => currency !== cur)
       this.sel = null
     },
-    selected(currency){
+    selected(currency) {
       this.sel = currency
       this.graph = []
     },
-    roundGraph(){
+    roundGraph() {
       const maxVal = Math.max(...this.graph)
       const minVal = Math.min(...this.graph)
-      return  this.graph.map((gr) => {
-        return  5+((gr - minVal) * 95)/(maxVal - minVal)
+      return this.graph.map((gr) => {
+        return 5 + ((gr - minVal) * 95) / (maxVal - minVal)
       })
     },
 
-    closeGraph(){
+    closeGraph() {
       this.sel = null
       clearInterval(this.interval)
-    }
+    },
 
+    async fetchCrypto() {
+      let response = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+      const data = await response.json()
+      this.allCrypto = data.Data
+    },
+
+    // createMainArr(obj, arr, param){
+    //   for (let key in obj) {
+    //     arr.push(key.param)
+    //   }
+    //   return arr
+    // }
+  },
+  mounted() {
+    this.fetchCrypto()
   }
 }
 </script>
