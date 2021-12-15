e93e0667a31900d1a7fc29524ff8bda38f401ba8
lismgmk <lismgmk2@gmail.com>
2021-12-15 20:01:58 +0300

    some prepare to bc

--- src/api.js
+++ src/api.js
@@ -2,29 +2,49 @@ const API_KEY = 'd52aa675ecbb2b0e1541ef3f89f1efbe2ab21a3bd943ef86e6041794d25b984
 
 const objCurrencyes = new Map()
 
-// const bc = new BroadcastChannel('gator_channel')
-//
-// bc.onmessage = (messageEvent) => {
-//     // If our broadcast message is 'update_title' then get the new title from localStorage
-//     if (messageEvent.data === 'update_title') {
-//         // localStorage is domain specific so when it changes in one window it changes in the other
-//         setTitle(localStorage.getItem('title'));
-//     }
-// }
-// bc.postMessage('update_title');
-
-
+const bc = new BroadcastChannel('gator_channel')
 const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`);
+const myWorker = new SharedWorker("worker.js");
+
+const obj = {
+    prices: 0,
+    cur: 0
+}
+
+bc.onmessage = (messageEvent) => {
+    if (messageEvent.data === 'update_title') {
+        // setTitle(localStorage.getItem('title'));
+        // socket.close()
+        const handlerCurrencyFunc = objCurrencyes.get(obj.cur) ?? []
+        handlerCurrencyFunc.forEach(fn => fn(obj.prices))
+
+    }
+}
+
+myWorker.port.addEventListener("message", function(e) {
+    alert(e.data);
+}, false);
+
+myWorker.port.start();
+
 const RESPONSE_PARAM = '5'
+const RESPONSE_ERROR_PARAM = '429'
 
 socket.addEventListener('message', function (event) {
     const {TYPE: type, PRICE: newPrice, FROMSYMBOL: newCurrency} = JSON.parse(event.data)
+    if(type === RESPONSE_ERROR_PARAM){
+        bc.postMessage({ type: "WSState", state: socket.readyState });
+       obj.prices = newPrice
+        obj.cur = newCurrency
+        console.log(obj)
+
+    }
     if (type !== RESPONSE_PARAM || newPrice === undefined) {
         return
     }
     const handlerCurrencyFunc = objCurrencyes.get(newCurrency) ?? []
     handlerCurrencyFunc.forEach(fn => fn(newPrice))
-
+bc.postMessage(obj)
 });
 
 
