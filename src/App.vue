eddcbede22b05118fe2684c5838f2c8d37249bce
lismgmk <lismgmk@gmail.com>
2022-01-04 18:04:19 +0300

    need fix dynamic props

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,59 @@
+<!--<section>-->
+<!--<div class="flex">-->
+<!--  <div class="max-w-xs">-->
+<!--    <label for="wallet" class="block text-sm font-medium text-gray-700"-->
+<!--    >Тикер</label-->
+<!--    >-->
+<!--    <div class="mt-1 relative rounded-md shadow-md">-->
+<!--      <input-->
+<!--          @keydown.enter="addCurrency(inputVal)"-->
+<!--          v-model="inputVal"-->
+<!--          @input="updtInput"-->
+<!--          type="text"-->
+<!--          name="wallet"-->
+<!--          id="wallet"-->
+<!--          class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"-->
+<!--          placeholder="Например DOGE"-->
+<!--      />-->
+<!--    </div>-->
+<!--    <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">-->
+<!--            <span-->
+<!--                @click="addCurrency(tag)"-->
+<!--                :key="tag"-->
+<!--                v-for="tag in tags"-->
+<!--                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">-->
+<!--              {{ tag }}-->
+<!--            </span>-->
+<!--    </div>-->
+<!--    <div v-show="flagDouble" class="text-sm text-red-600">Такой тикер уже добавлен</div>-->
+<!--  </div>-->
+<!--</div>-->
+<!--<button-->
+<!--    @click="addCurrency(inputVal)"-->
+<!--    type="button"-->
+<!--    class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"-->
+<!--&gt;-->
+<!--  <svg-->
+<!--      class="-ml-0.5 mr-2 h-6 w-6"-->
+<!--      xmlns="http://www.w3.org/2000/svg"-->
+<!--      width="30"-->
+<!--      height="30"-->
+<!--      viewBox="0 0 24 24"-->
+<!--      fill="#ffffff"-->
+<!--  >-->
+<!--    <path-->
+<!--        d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"-->
+<!--    ></path>-->
+<!--  </svg>-->
+<!--  Добавить-->
+<!--</button>-->
+<!--</section>-->
+
+
 <template>
   <div class="container" style="margin: 30px">
-    <section>
-      <div class="flex">
-        <div class="max-w-xs">
-          <label for="wallet" class="block text-sm font-medium text-gray-700"
-          >Тикер</label
-          >
-          <div class="mt-1 relative rounded-md shadow-md">
-            <input
-                @keydown.enter="addCurrency(inputVal)"
-                v-model="inputVal"
-                @input="updtInput"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
-            />
-          </div>
-          <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-            <span
-                @click="addCurrency(tag)"
-                :key="tag"
-                v-for="tag in tags"
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-              {{ tag }}
-            </span>
-          </div>
-          <div v-show="flagDouble" class="text-sm text-red-600">Такой тикер уже добавлен</div>
-        </div>
-      </div>
-      <button
-          @click="addCurrency(inputVal)"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-      >
-        <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-        >
-          <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-          ></path>
-        </svg>
-        Добавить
-      </button>
-    </section>
-<!--    <add-currency/>-->
+
+    <!--    <add-currency/>-->
 
     <template v-if="currencies.length">
       <div>
@@ -179,20 +182,20 @@
       </button>
     </section>
   </div>
-  <add-currency/>
-  <plus-single-icon />
+  <add-currency
+      v-on:inputFromAddCur="addCurrency"
+      v-bind:newInput='inputVal'
+  />
 </template>
 
 <script>
 import {getCurrensyTags, subscriberCurrecyes, unSubscriberCurrecyes} from "@/api";
 import AddCurrency from '@/components/AddCurrency.vue';
-import PlusSingleIcon from '@/components/PlusSignIcon.vue';
 
 export default {
   name: 'App',
-
   components: {
-    AddCurrency, PlusSingleIcon
+    AddCurrency
   },
 
   data() {
@@ -276,8 +279,27 @@ export default {
       return this.countGraph = this.$refs.graphArea.clientWidth / this.widthColumnGraph
     },
 
-    addCurrency(nameTag) {
-      let newCurrency = {price: '-', name: nameTag.toUpperCase(), empty: false}
+    // addCurrency(nameTag) {
+    //   let newCurrency = {price: '-', name: nameTag.toUpperCase(), empty: false}
+    //   if (this.currencies.filter(currency => currency.name === newCurrency.name).length === 0) {
+    //     this.currencies = [...this.currencies, newCurrency]
+    //     subscriberCurrecyes(
+    //         newCurrency.name,
+    //         (newPrice) => {
+    //           this.updateCurrecyes(newCurrency.name, newPrice)
+    //         }
+    //     )
+    //     this.inputVal = ''
+    //     this.flagDouble = false
+    //     this.tags = []
+    //   } else {
+    //     this.flagDouble = true
+    //     this.inputVal = nameTag
+    //   }
+    // },
+    addCurrency(newCurrency) {
+      console.log(newCurrency, '2 get recently input Val from child')
+      // let newCurrency = {price: '-', name: nameTag.toUpperCase(), empty: false}
       if (this.currencies.filter(currency => currency.name === newCurrency.name).length === 0) {
         this.currencies = [...this.currencies, newCurrency]
         subscriberCurrecyes(
@@ -286,12 +308,14 @@ export default {
               this.updateCurrecyes(newCurrency.name, newPrice)
             }
         )
+
         this.inputVal = ''
+        console.log(this.inputVal)
         this.flagDouble = false
         this.tags = []
       } else {
         this.flagDouble = true
-        this.inputVal = nameTag
+        this.inputVal = newCurrency
       }
     },
 
@@ -380,6 +404,10 @@ export default {
 
 
   watch: {
+    inputVal(){
+      console.log(this.inputVal, 'watch input')
+      return this.inputVal
+    },
     sel() {
       this.graph = []
       this.$nextTick().then(this.countNumberGraph)
--- src/components/AddCurrency.vue
+++ src/components/AddCurrency.vue
@@ -1,57 +1,84 @@
 <template>
   <section>
-<!--    <h1>New test</h1>-->
-<!--    <div class="flex">-->
-<!--      <div class="max-w-xs">-->
-<!--        <label for="wallet" class="block text-sm font-medium text-gray-700"-->
-<!--        >Тикер</label-->
-<!--        >-->
-<!--        <div class="mt-1 relative rounded-md shadow-md">-->
-<!--          <input-->
-<!--              @keydown.enter="addCurrency(inputVal)"-->
-<!--              v-model="inputVal"-->
-<!--              @input="updtInput"-->
-<!--              type="text"-->
-<!--              name="wallet"-->
-<!--              id="wallet"-->
-<!--              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"-->
-<!--              placeholder="Например DOGE"-->
-<!--          />-->
-<!--        </div>-->
-<!--        <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">-->
-<!--            <span-->
-<!--                @click="addCurrency(tag)"-->
-<!--                :key="tag"-->
-<!--                v-for="tag in tags"-->
-<!--                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">-->
-<!--              {{ tag }}-->
-<!--            </span>-->
-<!--        </div>-->
-<!--        <div v-show="flagDouble" class="text-sm text-red-600">Такой тикер уже добавлен</div>-->
-<!--      </div>-->
-<!--    </div>-->
-    <button
-        @click="addCurrency(inputVal)"
-        type="button"
-        class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-    >
-      <svg
-          class="-ml-0.5 mr-2 h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 24 24"
-          fill="#ffffff"
-      >
-        <path
-            d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-        ></path>
-      </svg>
-      Добавить
-    </button>
+    <h1>New test</h1>
+    <div class="flex">
+      <div class="max-w-xs">
+        <label for="wallet" class="block text-sm font-medium text-gray-700"
+        >Тикер</label
+        >
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input
+              @keydown.enter="addCurrency"
+              v-model="inputVal"
+              @input="updtInput"
+              type="text"
+              name="wallet"
+              id="wallet"
+              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+              placeholder="Например DOGE"
+          />
+        </div>
+        <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+            <span
+                @click="addCurrency(tag)"
+                :key="tag"
+                v-for="tag in tags"
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              {{ tag }}
+            </span>
+        </div>
+        <div v-show="flagDouble" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+      </div>
+    </div>
+
+    <plus-single-icon
+        v-on:click = "$emit('inputFromAddCur', addOnlyValue())"/>
   </section>
 </template>
 <script>
+import PlusSingleIcon from './PlusSignIcon.vue'
+
+export default {
+  name: 'AddCurrency',
+  props: ['newInput'],
+  components: {
+    PlusSingleIcon
+  },
+  data(){
+    return {
+      inputVal: ''
+    }
+  },
+  watch:{
+    newInput: function(newVal) {
+      console.log(newVal, '3 watch new props ')
+      this.inputVal = newVal
+    }
+  },
+  methods: {
+    addOnlyValue() {
+      let k = {price: '-', name: this.inputVal.toUpperCase(), empty: false}
+      console.log(k, '1 send to App Obj')
+      return  {price: '-', name: this.inputVal.toUpperCase(), empty: false}
+
+      // if (this.currencies.filter(currency => currency.name === newCurrency.name).length === 0) {
+      //   this.currencies = [...this.currencies, newCurrency]
+      //   subscriberCurrecyes(
+      //       newCurrency.name,
+      //       (newPrice) => {
+      //         this.updateCurrecyes(newCurrency.name, newPrice)
+      //       }
+      //   )
+      //   this.inputVal = ''
+      //   this.flagDouble = false
+      //   this.tags = []
+      // } else {
+      //   this.flagDouble = true
+      //   this.inputVal = nameTag
+      // }
+    },
+  }
+}
 
 
 </script>
